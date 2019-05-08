42b4fb347c171bca9671cc4c6c68b3ddc67cd85b
aramire4 <aramire4@europa.cs.trinity.edu>
2019-05-08 12:39:50 -0500

    able to display all tamagos on a user profile page

--- client/src/main/scala/edu/trinity/webapps/ProfilePage.scala
+++ client/src/main/scala/edu/trinity/webapps/ProfilePage.scala
@@ -30,33 +30,77 @@ object ProfilePage {
     val canvas = dom.document.getElementById("petCenter").asInstanceOf[dom.raw.HTMLCanvasElement]
     val context = canvas.getContext("2d")
     $.getJSON("/tamagos", success = (o, s, j) => {
+      //println("1")
       for (t <- Json.parse(js.JSON.stringify(o)).as[Array[TamagoData]]) {
         val tamaPar = $(s"<p>name: ${t.name}, age: ${t.age}, health: ${t.health}</p>")
+        //put a link on name to CurrentPet
         $("#profile-page").append(tamaPar)
         Player.tamagos ::= t
-        /*
-        context.fillRect(50,50,50,50)
-        context.fillText("name: "+t.name, 200, 200)
-        */
       }
+      showTamago();
     })
-    var count = 0
-    for (t <- Player.tamagos) {
-      t.age match {
-        case 1 => {
-          //val img = dom.document.getElementById("tomahat").asInstanceOf[HTMLImageElement]
-          val clone = $("tomahat").clone()
-          $(clone).attr("id", "pic" + count.toString)
-          $("pic" + count.toString).removeAttr("style")
-          $("#profile-page").append($(clone))
-          clone.click(() => CurrentPet.pageSetup(t))
-        }
-
-      }
-      count += 1
-    }
     //var image = dom.document.getElementById("tomahat").asInstanceOf[HTMLImageElement]
     //context.drawImage(image, 50, 50)
+  }
+
+  def showTamago(): Unit = {
+    val canvas = dom.document.getElementById("petCenter").asInstanceOf[dom.raw.HTMLCanvasElement]
+    val context = canvas.getContext("2d")
+    var x = 50;
+    var y = 50;
+    for (t <- Player.tamagos) {
+      var image = dom.document.getElementById("tomahat").asInstanceOf[HTMLImageElement]
+      t.age match {
+        case 1 => {
+          image = dom.document.getElementById("tomahat").asInstanceOf[HTMLImageElement]
+        }
+        case 2 => {
+          image = dom.document.getElementById("tomaflap").asInstanceOf[HTMLImageElement]
+        }
+        case 3 => {
+          image = dom.document.getElementById("tomamuscle").asInstanceOf[HTMLImageElement]
+        }
+        case 4 => {
+          image = dom.document.getElementById("tomamark").asInstanceOf[HTMLImageElement]
+        }
+        case 5 =>{
+          image = dom.document.getElementById("tomasad").asInstanceOf[HTMLImageElement]
+        }
+        case 6 =>{
+          image = dom.document.getElementById("tomastache").asInstanceOf[HTMLImageElement]
+        }
+        case 7 =>{
+          image = dom.document.getElementById("tomagib").asInstanceOf[HTMLImageElement]
+        }
+        case 8 =>{
+          image = dom.document.getElementById("tomagusta").asInstanceOf[HTMLImageElement]
+        }
+        case 9 =>{
+          image = dom.document.getElementById("tomahawk").asInstanceOf[HTMLImageElement]
+        }
+        case 10 =>{
+          image = dom.document.getElementById("tomasprout").asInstanceOf[HTMLImageElement]
+        }
+        case 11 =>{
+          image = dom.document.getElementById("tomamystery").asInstanceOf[HTMLImageElement]
+        }
+        case 12 =>{
+          image = dom.document.getElementById("tomaskitters").asInstanceOf[HTMLImageElement]
+        }
+        case _ => {
+          println("This is an error")
+        }
+      }
+      context.drawImage(image, x, y)
+      context.font = "30px Arial";
+      context.fillText(t.name, x + 15, y);
+      
+      x += 200
+      if(x > 1400){
+        x = 50
+        y += 200
+      }
+    }
   }
 
   def addPlayer(): Unit = {
