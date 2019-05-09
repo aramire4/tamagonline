7131eb131e490e23aca61b9d2aa36ec75707643f
dmatthe1 <dmatthe1@trinity.edu>
2019-05-09 10:45:42 -0500

    tony is so dumb

---
+++ client/src/main/scala/edu/trinity/webapps/Battle.scala
@@ -0,0 +1,46 @@
+package edu.trinity.webapps
+
+import org.scalajs.dom
+import scala.scalajs.js
+import org.querki.jquery._
+
+import scala.scalajs.js.annotation._
+import play.api.libs.json._
+import play.api.libs.json.Reads._
+import play.api.libs.functional.syntax._
+import edu.trinity.webapps.shared.SharedTables._
+import org.scalajs.dom.html.Canvas
+import org.scalajs.dom.raw.HTMLImageElement
+
+//note to self - query stuff is a post/get, meaning you can keep sessions with that?!!??
+
+object Battle {
+
+  def pageSetup(): Unit = {
+    Player.clearData()
+    $("#main-body").empty()
+    $("#main-body").append($("<div id=\"profile-page\"></div>"))
+    $("#profile").click(() => ProfilePage.pageSetup())
+    $("#store").click(() => Store.pageSetup())
+    $("#help").click(() => Help.pageSetup())
+  }
+  
+  val str = """
+<span>
+	<body>
+		<div class="topnav">
+			<a id="profile">Profile</a> 
+			<a id="store">Store</a> 
+			<a id="help">Help</a>
+    </div>
+
+		<h2>Profile</h2>
+		<p class = "center"> Below are all of your tamagos! Click on one to interact with it. </p>
+		<canvas id="petCenter" width="1400" height="600"
+			style="border: 3px solid"></canvas>
+		<br>
+  <ul id="pets" class="center"> <h3> Tamago List </h3> </ul>
+	</body>
+</span>"""
+
+}
---
+++ client/src/main/scala/edu/trinity/webapps/Frolic.scala
@@ -0,0 +1,45 @@
+package edu.trinity.webapps
+
+import org.scalajs.dom
+import scala.scalajs.js
+import org.querki.jquery._
+
+import scala.scalajs.js.annotation._
+import play.api.libs.json._
+import play.api.libs.json.Reads._
+import play.api.libs.functional.syntax._
+import edu.trinity.webapps.shared.SharedTables._
+import org.scalajs.dom.html.Canvas
+import org.scalajs.dom.raw.HTMLImageElement
+
+//note to self - query stuff is a post/get, meaning you can keep sessions with that?!!??
+
+object Frolic {
+
+  def pageSetup(): Unit = {
+    Player.clearData()
+    $("#main-body").empty()
+    $("#main-body").append($("<div id=\"profile-page\"></div>"))
+    $("#profile").click(() => ProfilePage.pageSetup())
+    $("#store").click(() => Store.pageSetup())
+    $("#help").click(() => Help.pageSetup())
+  }
+
+  val str = """
+<span>
+	<body>
+		<div class="topnav">
+			<a id="profile">Profile</a> 
+			<a id="store">Store</a> 
+			<a id="help">Help</a>
+    </div>
+
+		<h2>Frolic</h2>
+		<canvas id="petCenter" width="1400" height="600"
+			style="border: 3px solid"></canvas>
+		<br>
+  <ul id="pets" class="center"> <h3> Tamago List </h3> </ul>
+	</body>
+</span>"""
+
+}
